25889033b15f96f8fa7fe55382d58760dc57fc9f
h5lianfu <h5lianfu@gmail.com>
2025-10-01 17:43:50 -0400

    add index.html

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,6 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  server: { port: 5055 },
+  html: { template: './public/index.html' },
 });
