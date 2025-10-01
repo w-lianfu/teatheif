7b2fde25d1ec7dde0a689a7ea79acdfcfa7f8515
h5lianfu <h5lianfu@gmail.com>
2025-10-01 17:33:44 -0400

    little update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import './App.css';
-
 const App = () => {
   return (
-    <div className="content">
+    <div>
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
